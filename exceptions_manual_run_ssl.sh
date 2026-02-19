6a729ef9efbe366c2f28e77e39657fdb5fcbca1d
Maksim Slivka <94443666+Mak2055@users.noreply.github.com>
2026-02-19 14:12:26 +0100

    Update exceptions_manual_run_ssl.sh
    Add a comment

--- exceptions_manual_run_ssl.sh
+++ exceptions_manual_run_ssl.sh
@@ -20,7 +20,7 @@ f_fmap="sub-*_ses-*_acq-tr1_run-?_TB1AFI.nii.gz" # acq-tr1 for AFI; acq-famp for
 dir_tool='/path/to/SSL-QALAS-main-crossvendor'   # Path to the folder where SSL-QALAS-main-crossvendor is stored
 dir_bids='/path/to/bids'                         # Path to BIDS where all the participants to be processed are stored
 afi_out=$dir_tool'/afi_b1_maps'                  # Path to the folder where estimated AFI maps should be saved (if applicable) - can be within the tool folder
-dir_conda='/path/to/conda'                       # Path to (mini)conda on your machine
+dir_conda='/path/to/conda'                       # Path to (mini)conda or to a standalone environment directory (if the environment is not registered in Conda, see Troubleshooting in README.md).
 dir_matlab='/path/to/MATLAB'                     # Path to MATLAB on your machine
 lic_matlab=''                                    # Leave empty if the licence is provided in MATLAB folder (most likely scenario), otherwise provide the license file or the license server
 
